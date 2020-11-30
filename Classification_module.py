d271a797cc93c09d6aef5b04c55139e38519e12f
IamWen <shiwen0728@gmail.com>
2020-11-30 01:21:12 -0800

    Organized functions

--- Classification_module.py
+++ Classification_module.py
@@ -1,14 +1,36 @@
+import sys
+sys.setrecursionlimit(10000)
+
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split, LeavePGroupsOut, LeaveOneGroupOut, GroupShuffleSplit,cross_val_score
+from sklearn.model_selection import train_test_split, GridSearchCV, LeavePGroupsOut, LeaveOneGroupOut, GroupShuffleSplit,cross_val_score
 from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+import tensorflow.keras as keras
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Dense, Conv2D, Dropout, MaxPooling2D, Flatten
 
 import ReadFiles as RF
 import ExtractFeatures as EF
+
+
+def preprocessing(feature_df):
+    # Check for null
+    null_df = pd.DataFrame(feature_df.isnull().mean(), columns=["null %"])
+    null_cols = null_df[null_df["null %"] > 0]
+    # View cleaned dataframe
+    cleaned_feature_df = feature_df.drop(list(null_cols.index), axis=1)
+    cleaned_feature_df.head()
+    return cleaned_feature_df
+
 
 def normalize_data(feature_train, feature_test):
     scaler = StandardScaler().fit(feature_train)
@@ -18,16 +40,6 @@ def normalize_data(feature_train, feature_test):
     feature_train2 = scaler.transform(feature_train)
     feature_test2 = scaler.transform(feature_test)
     return feature_train2, feature_test2, mu, std
-
-
-def cross_validation(X, y, IDs):
-    logo = LeaveOneGroupOut()
-
-    for train_index, val_index in logo.split(X, y, groups=IDs.ravel()):
-        X_train, X_val = X[train_index], X[val_index]
-        y_train, y_val = y[train_index], y[val_index]
-        SVM_classifier(X_train, X_val, y_train, y_val)
-    return
 
 
 def split_randomly(X, y):
@@ -54,6 +66,26 @@ def split_by_ID(X, y, IDs):
 
         # cross_validation(X_train,y_train,IDs[train_index])
         SVM_classifier(X_train, X_test, y_train, y_test)
+    return
+
+
+def cross_validation(X, y, IDs):
+    logo = LeaveOneGroupOut()
+
+    for train_index, val_index in logo.split(X, y, groups=IDs.ravel()):
+        X_train, X_val = X[train_index], X[val_index]
+        y_train, y_val = y[train_index], y[val_index]
+        SVM_classifier(X_train, X_val, y_train, y_val)
+    return
+
+
+def Logistic_regression(X_train, X_test, y_train, y_test):
+    log_model = LogisticRegression()
+    log_model.fit(X_train, y_train)
+    train_preds = log_model.predict(X_train)
+    test_preds = log_model.predict(X_test)
+    print("Training Accuracy: %f" % accuracy_score(train_preds, y_train))
+    print("Testing Accuracy: %f" % accuracy_score(test_preds, y_test))
     return
 
 
@@ -111,8 +143,18 @@ def random_forest(X_train, X_test, y_train, y_test):
     plt.show()
 
 
+def AdaBoost(X_train, X_test, y_train, y_test):
+    adaboost_model = AdaBoostClassifier()
+    adaboost_model.fit(X_train, y_train)
+    train_preds = adaboost_model.predict(X_train)
+    test_preds = adaboost_model.predict(X_test)
+    print("Training Accuracy: %f" % accuracy_score(train_preds, y_train))
+    print("Testing Accuracy: %f" % accuracy_score(test_preds, y_test))
+    return
+
+
 def classify_data(X,y):
-    X_train, X_val, X_test, y_train, y_val, y_test = split_randomly(X,y)
+
     SVM_classifier(X_train, X_test, y_train, y_test)
     random_forest(X_train, X_test, y_train, y_test)
 
@@ -120,8 +162,9 @@ def classify_data(X,y):
 if __name__ == "__main__":
     dir = 'E:/USC/EE660_2020/data'
     eeg_epoch_full_df, W1, W2 = RF.read_epoched_data(dir)
-    feature_array = EF.neuroDSP_alpha_instantaneous_amplitude_median(W1, eeg_epoch_full_df)
-    y = feature_array[['y']]
-    X = feature_array[feature_array.columns[~feature_array.columns.isin(['y'])]]
-
+    feature_df = EF.neuroDSP_alpha_instantaneous_amplitude_median(W1, eeg_epoch_full_df)
+    cleaned_feature_df = preprocessing(feature_df)
+    y = feature_df["y"]
+    X = feature_df.drop("y",axis=1)
+    X_train, X_val, X_test, y_train, y_val, y_test = split_randomly(X,y)
     classify_data(X,y)
--- ExtractFeatures.py
+++ ExtractFeatures.py
@@ -17,6 +17,167 @@ all_chans = eeg_chans + eog_chans
 event_types = {0:"left", 1:"right"}
 
 
+# PSD plotting
+def plotPSD(freq, psd, fs=eeg_fs, pre_cut_off_freq=0, post_cut_off_freq=120, label=None):
+    '''
+    Inputs
+    - freq: the list of frequencies corresponding to the PSDs
+    - psd: the list of psds that represent the power of each frequency
+    - pre_cut_off_freq: the lowerbound of the frequencies to show
+    - post_cut_off_freq: the upperbound of the frequencies to show
+    - label: a text label to assign this plot (in case multiple plots want to be drawn)
+
+    Outputs:
+    - None, except a plot will appear. plot.show() is not called at the end, so you can call this again to plot on the same axes.
+    '''
+    # Label the axes
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('log(PSD)')
+
+    # Calculate the frequency point that corresponds with the desired cut off frequencies
+    pre_cut = int(len(freq) * (pre_cut_off_freq / freq[-1]))
+    post_cut = int(len(freq) * (post_cut_off_freq / freq[-1]))
+
+    # Plot
+    plt.plot(freq[pre_cut:post_cut], np.log(psd[pre_cut:post_cut]), label=label)
+
+
+# Get Frequencies and PSDs from EEG data - this is the raw PSD method.
+def getFreqPSDFromEEG(eeg_data, fs=eeg_fs):
+    # Use scipy's signal.periodogram to do the conversion to PSDs
+    freq, psd = signal.periodogram(eeg_data, fs=int(fs), scaling='spectrum')
+    return freq, psd
+
+# Get Frequencies and mean PSDs from EEG data - this yeilds smoother PSDs because it averages the PSDs made from sliding windows.
+def getMeanFreqPSD(eeg_data, fs=eeg_fs):
+    freq_mean, psd_mean = compute_spectrum(eeg_data, fs, method='welch', avg_type='mean', nperseg=fs*2)
+    return freq_mean, psd_mean
+
+# Plot PSD from EEG data (combines the a PSD calculator function and the plotting function)
+def plotPSD_fromEEG(eeg_data, fs=eeg_fs, pre_cut_off_freq=0, post_cut_off_freq=120, label=None):
+    freq, psd = getMeanFreqPSD(eeg_data, fs=fs)
+    plotPSD(freq, psd, fs, pre_cut_off_freq, post_cut_off_freq, label)
+
+# Spectrogram plotting
+def plotSpectrogram_fromEEG(eeg_data, fs=eeg_fs, pre_cut_off_freq=0, post_cut_off_freq=120):
+    f, t, Sxx = signal.spectrogram(eeg_data, fs=fs)
+    # Calculate the frequency point that corresponds with the desired cut off frequencies
+    pre_cut = int(len(f) * (pre_cut_off_freq / f[-1]))
+    post_cut = int(len(f) * (post_cut_off_freq / f[-1]))
+    plt.pcolormesh(t, f[pre_cut:post_cut], Sxx[pre_cut:post_cut], shading='gouraud')
+    plt.ylabel("Frequency (Hz)")
+    plt.xlabel("Time (sec)")
+
+
+def plot_continuous_data_by_timepoint(eeg_df):
+    # Try adjust these variables to see different time ranges!
+    # A single trial is 4 seconds or 1000 timpoints (4 ms per timepoint)
+    # Hint: refer to the Epoched data dataframe for the time of each trial
+    start_time_ms = 223556  # Start time in millis
+    start_time_timepoints = start_time_ms // 4  # Divide by 4 to get into timepoints
+    end_time_timepoints = start_time_timepoints + 1000  # Specify number of more timepoints we want past start
+
+    # Plot a single EEG channel
+    plt.figure(figsize=(15, 5))
+    plt.plot(eeg_df['C3'].values[start_time_timepoints:end_time_timepoints])
+    plt.title("C3 -- " + str(start_time_timepoints) + " to " + str(end_time_timepoints))
+    plt.xlabel("timepoints")
+    plt.ylabel("Voltage (uV)")
+    plt.show()
+
+    # Plot a single EOG channel
+    plt.figure(figsize=(15, 5))
+    plt.plot(eeg_df['EOG:ch01'].values[start_time_timepoints:end_time_timepoints])
+    plt.title("EOG:ch01 -- " + str(start_time_timepoints) + " to " + str(end_time_timepoints))
+    plt.xlabel("timepoints")
+    plt.ylabel("Voltage (uV)")
+    plt.show()
+
+    # Plot the PSD of the single EEG channel
+    plt.figure(figsize=(15, 5))
+    plotPSD_fromEEG(eeg_df['C3'].values[start_time_timepoints:end_time_timepoints], pre_cut_off_freq=2,
+                    post_cut_off_freq=30, label="C3")
+    plt.title("PSD of C3 in the timespan provided")
+    plt.legend()
+    plt.show()
+
+    # Plot the spectrogram of the single EEG channel
+    plt.figure(figsize=(15, 5))
+    plotSpectrogram_fromEEG(eeg_df['C3'].values[start_time_timepoints:end_time_timepoints], pre_cut_off_freq=2,
+                            post_cut_off_freq=30)
+    plt.title("Spectrogram of C3 in the timespan provided")
+    plt.show()
+
+
+def plot_1_trial(eeg_epoch_full_df):
+    # Visualize EEG and PSD for one trial
+    # Try changing trial_num to view different trials!
+    trial_num = 0
+    eeg_chans = ["C3", "Cz", "C4"]  # 10-20 system
+    eog_chans = ["EOG:ch01", "EOG:ch02", "EOG:ch03"]
+
+    plt.figure(figsize=(15, 5))
+    for ch in eeg_chans:
+        plt.plot(eeg_epoch_full_df[ch][trial_num], label=ch)
+    plt.ylabel("Voltage (uV)")
+    plt.xlabel("timepoints @ 250Hz")
+    plt.title("EEG of one motor imagery trial")
+    plt.legend()
+    plt.show()
+
+    plt.figure(figsize=(15, 5))
+    for ch in eog_chans:
+        plt.plot(eeg_epoch_full_df[ch][trial_num], label=ch)
+    plt.ylabel("Voltage (uV)")
+    plt.xlabel("timepoints @ 250Hz")
+    plt.title("EOG of one motor imagery trial")
+    plt.legend()
+    plt.show()
+
+    plt.figure(figsize=(15, 5))
+    for ch in eeg_chans:
+        plotPSD_fromEEG(eeg_epoch_full_df[ch][trial_num], pre_cut_off_freq=2, post_cut_off_freq=30, label=ch)
+    plt.title("PSD of EEG in one motor imagery trial")
+    plt.legend()
+    plt.show()
+
+
+def get_PSD_avg(eeg_epoch_full_df):
+    # Get PSD averages for each channel for each event type (0=left or 1=right)
+    eeg_chans = ["C3", "Cz", "C4"]  # 10-20 system
+    eog_chans = ["EOG:ch01", "EOG:ch02", "EOG:ch03"]
+    event_types = {0: "left", 1: "right"}
+
+    psd_averages_by_type = {}
+    for event_type in event_types.keys():
+        psds_only_one_type={}
+        freqs_only_one_type={}
+        for i, row in eeg_epoch_full_df[eeg_epoch_full_df["event_type"] == event_type].iterrows():
+            for ch in eeg_chans:
+                if ch not in psds_only_one_type:
+                    psds_only_one_type[ch] = list()
+                    freqs_only_one_type[ch] = list()
+                f, p = getMeanFreqPSD(row[ch])
+                psds_only_one_type[ch].append(p)
+                freqs_only_one_type[ch].append(f)
+        avg_psds_one_type = {}
+        for ch in eeg_chans:
+            psds_only_one_type[ch] = np.array(psds_only_one_type[ch])
+            avg_psds_one_type[ch] = np.mean(psds_only_one_type[ch], axis=0)
+        psd_averages_by_type[event_type] = dict(avg_psds_one_type)
+
+    # View Average PSDs
+    for event_type in event_types.keys():
+        for ch in eeg_chans[:]:
+            plotPSD(freqs_only_one_type[eeg_chans[0]][0], psd_averages_by_type[event_type][ch], pre_cut_off_freq=2,
+                    post_cut_off_freq=30, label=ch)
+
+        plt.legend()
+        plt.title("event type: " + event_types[event_type])
+        plt.show()
+
+
+
 # Multiple bar graph plotting
 def plotMultipleBarGraphs(bars, bar_width, bar_names, group_names, error_values=None, title=None, xlabel=None,
                           ylabel=None):
@@ -103,6 +264,8 @@ def getPowerBin(eeg_epoch_full_df):
     std_err_df = pd.DataFrame(chan_frequency_sems)
     avg_df = pd.DataFrame(chan_frequency_avgs)
 
+
+def plot_avg_PowerRatios():
     # Plot average power ratios for each electrode
     for chan in eeg_chans:
         chan_of_interest = chan
@@ -129,7 +292,7 @@ def getPowerBin(eeg_epoch_full_df):
                               error_values=np.transpose(np.array(event_sems_df)))
 
 
-def PowerBandRatios(power_ratios_df):
+def getPowerBandRatios(power_ratios_df):
     ## Get band ratio features
     theta_beta_ratios = {}  # Keys will be chan_theta_beta
     # Iterate through rows of power_ratios_df
@@ -151,7 +314,10 @@ def PowerBandRatios(power_ratios_df):
 
     # Concatenate power_ratios_df with band_ratios_df to get full feature df
     feature_df = pd.concat([power_ratios_df, band_ratios_df], axis=1)
+    return power_ratios_df, feature_df
 
+
+def get_channel_relationships(power_ratios_df, band_ratios_df):
     ## Get channel ratio features
     # Similar algorithm as Band Ratios, but for each interval
     C3_C4_differences = {}
@@ -179,25 +345,44 @@ def PowerBandRatios(power_ratios_df):
 def neuroDSP_alpha_instantaneous_amplitude_median(W1_feature_df, eeg_epoch_full_df):
     alpha_range = (7, 12)
     alpha_amps = {}
+    alpha_pha = {}
+    alpha_if = {}
+
     for i in range(0, len(eeg_epoch_full_df)):
         for ch in eeg_chans:
-            amp = amp_by_time(eeg_epoch_full_df[ch][i][:], eeg_fs, alpha_range)
+            sig = eeg_epoch_full_df[ch][i][:]
             key = ch + "_" + str(alpha_range) + "_inst_med"
+
+            amp = amp_by_time(sig, eeg_fs, alpha_range) # Amplitude by time (instantaneous amplitude)
             if key not in alpha_amps:
                 alpha_amps[key] = list()
             alpha_amps[key].append(np.nanmedian(amp))
 
+            pha = phase_by_time(sig, eeg_fs, alpha_range) # Phase by time (instantaneous phase)
+            if key not in alpha_pha:
+                alpha_pha[key] = list()
+            alpha_pha[key].append(np.nanmedian(pha))
+
+            i_f = freq_by_time(sig, eeg_fs, alpha_range) # Frequency by time (instantaneous frequency)
+            if key not in alpha_if:
+                alpha_if[key] = list()
+            alpha_if[key].append(np.nanmedian(i_f))
+
+
     alpha_med_df = pd.DataFrame(alpha_amps)
-    feature_df = pd.concat([W1_feature_df, alpha_med_df], axis=1)
+    alpha_pha_df = pd.DataFrame(alpha_pha)
+    alpha_if_df = pd.DataFrame(alpha_if)
+    feature_df = pd.concat([W1_feature_df, alpha_med_df, alpha_pha_df,alpha_if_df], axis=1)
     return feature_df
 
 
-def FOOOF():
+def FOOOF(eeg_epoch_full_df, W1_feature_df):
     # Import required code for visualizing example models
     from fooof import FOOOF
     from fooof.sim.gen import gen_power_spectrum
     from fooof.sim.utils import set_random_seed
     from fooof.plts.annotate import plot_annotated_model
+
 
     # Set random seed, for consistency generating simulated data
     set_random_seed(10)
@@ -211,3 +396,112 @@ def FOOOF():
     fm1.fit(freqs1, powers1)
 
     plot_annotated_model(fm1, annotate_aperiodic=True)
+    # Constants
+    freq_range = [1, 40]
+    alpha_range = (7, 12)
+    # Initialize a FOOOF object
+    fm = FOOOF(peak_width_limits=[1, 8], max_n_peaks=6, min_peak_height=0.4)
+
+    # Get the PSD of our EEG Signal
+    sig = eeg_epoch_full_df['Cz'][0]
+    freq, psd = getMeanFreqPSD(sig)
+
+    fm.add_data(freq, psd, freq_range)
+
+    fm.fit()
+    fm.report()
+
+    # Get PSD averages for each channel for each event type (0=left or 1=right)
+    psd_averages_by_type = {}
+
+    for event_type in event_types.keys():
+        psds_only_one_type = {}
+        freqs_only_one_type = {}
+        for i, row in eeg_epoch_full_df[eeg_epoch_full_df["event_type"] == event_type].iterrows():
+            for ch in eeg_chans:
+                if ch not in psds_only_one_type:
+                    psds_only_one_type[ch] = list()
+                    freqs_only_one_type[ch] = list()
+                f, p = getMeanFreqPSD(row[ch])
+                psds_only_one_type[ch].append(p)
+                freqs_only_one_type[ch].append(f)
+        avg_psds_one_type = {}
+        for ch in eeg_chans:
+            psds_only_one_type[ch] = np.array(psds_only_one_type[ch])
+            avg_psds_one_type[ch] = np.mean(psds_only_one_type[ch], axis=0)
+        psd_averages_by_type[event_type] = dict(avg_psds_one_type)
+
+    # Visualize the parameters in these two classes of C4 activity
+    fm_left_C4 = FOOOF(peak_width_limits=[1, 8], max_n_peaks=6, min_peak_height=0.4)
+    fm_left_C4.add_data(freqs_only_one_type[eeg_chans[0]][0], psd_averages_by_type[0]['C4'], freq_range)
+    fm_left_C4.fit()
+    fm_left_C4.report()
+
+    fm_right_C4 = FOOOF(peak_width_limits=[1, 8], max_n_peaks=6, min_peak_height=0.4)
+    fm_right_C4.add_data(freqs_only_one_type[eeg_chans[0]][0], psd_averages_by_type[1]['C4'], freq_range)
+    fm_right_C4.fit()
+    fm_right_C4.report()
+
+
+    # Calculate central freq, alpha power, and bandwidth for each channel and each trial
+    # This cell takes a few minutes to run (~8 mins on my computer). There are 3680 trials in the training data.
+
+    # Initialize a fooof object
+    fm = FOOOF(peak_width_limits=[1, 8], max_n_peaks=6, min_peak_height=0.4)
+
+    # Some will not have alpha peaks, use these variables to keep track
+    num_with_alpha = 0
+    num_without_alpha = 0
+    fooof_parameters = {}
+    for i in range(len(eeg_epoch_full_df)):
+        # Print the trial number every 100 to make sure we're making progress
+        if i % 100 == 0:
+            print(i)
+        for ch in eeg_chans:
+            # Determine the key
+            CF_key = ch + "_alpha_central_freq"
+            PW_key = ch + "_alpha_power"
+            BW_key = ch + "_alpha_band_width"
+            if CF_key not in fooof_parameters:
+                fooof_parameters[CF_key] = []
+            if PW_key not in fooof_parameters:
+                fooof_parameters[PW_key] = []
+            if BW_key not in fooof_parameters:
+                fooof_parameters[BW_key] = []
+
+            # Calculate the PSD for the desired signal
+            sig = eeg_epoch_full_df[ch][i]
+            freq, psd = getMeanFreqPSD(sig)
+
+            # Set the frequency and spectral data into the FOOOF model and get peak params
+            fm.add_data(freq, psd, freq_range)
+            fm.fit()
+            peak_params = fm.peak_params_
+
+            # Only select the peaks within alpha power
+            peak_params_alpha = []
+            for param in peak_params:
+                if (param[0] > alpha_range[0]) and (param[0] < alpha_range[1]):
+                    peak_params_alpha.append(param)
+
+            # Take the average if there are multiple peaks detected, otherwise 0 everything
+            means = []
+            if len(peak_params_alpha) > 0:
+                num_with_alpha += 1
+                means = np.mean(peak_params_alpha, axis=0)
+            else:
+                num_without_alpha += 1
+                means = [0, 0, 0]
+
+            fooof_parameters[CF_key].append(means[0])
+            fooof_parameters[PW_key].append(means[1])
+            fooof_parameters[BW_key].append(means[2])
+
+    # Concatenate
+    fooof_parameters_df = pd.DataFrame(fooof_parameters)
+
+    feature_df = pd.concat([W1_feature_df, fooof_parameters_df], axis=1)
+    print("% with alpha:", num_with_alpha / (num_with_alpha + num_without_alpha))
+
+    # Save it so you don't need to spend extra time rerunning the heavy computation cell from above.
+    feature_df.to_pickle("W2_feature_df.pkl")
